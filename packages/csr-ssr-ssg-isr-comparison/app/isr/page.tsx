98717b150df45f5c5bba5e67232a5a4d8c39b503
dobin <dobinshin@gmail.com>
2025-04-03 14:18:46 +0900

    c: add RouteToHome button component

--- packages/csr-ssr-ssg-isr-comparison/app/isr/page.tsx
+++ packages/csr-ssr-ssg-isr-comparison/app/isr/page.tsx
@@ -1,5 +1,6 @@
 // app/isr/page.tsx
 import DataDisplay from '@/components/DataDisplay';
+import RouteToHome from '@/components/RouteToHome';
 
 // Incremental Static Regeneration (ISR) – This function fetches data at build time and updates the cache every 60 seconds
 export default async function ISRPage() {
@@ -11,5 +12,12 @@ export default async function ISRPage() {
     const data = await res.json();
 
     // Render the page with the fetched data
-    return <DataDisplay data={data} />;
+    return (
+        // display the data and a button that navigates to the home page
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">ISR</h1>
+            <DataDisplay data={data} />
+            <RouteToHome />
+        </main>
+    );
 }
--- packages/csr-ssr-ssg-isr-comparison/app/page.tsx
+++ packages/csr-ssr-ssg-isr-comparison/app/page.tsx
@@ -1,21 +1,23 @@
+import Link from 'next/link';
+
 // page has 4 buttons it self route to /csr, /ssr, /ssg, /isr
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-4xl font-bold">CSR vs SSR vs SSG vs ISR</h1>
             <div className="flex gap-4 mt-4">
-                <a href="/csr" className="btn">
-                    CSR
-                </a>
-                <a href="/ssr" className="btn">
-                    SSR
-                </a>
-                <a href="/ssg" className="btn">
-                    SSG
-                </a>
-                <a href="/isr" className="btn">
-                    ISR
-                </a>
+                <div className="flex justify-center mt-4 border p-2 rounded-xs cursor-pointer">
+                    <Link href="/csr">CSR</Link>
+                </div>
+                <div className="flex justify-center mt-4 border p-2 rounded-xs cursor-pointer">
+                    <Link href="/ssr">SSR</Link>
+                </div>
+                <div className="flex justify-center mt-4 border p-2 rounded-xs cursor-pointer">
+                    <Link href="/ssg">SSG</Link>
+                </div>
+                <div className="flex justify-center mt-4 border p-2 rounded-xs cursor-pointer">
+                    <Link href="/isr">ISR</Link>
+                </div>
             </div>
         </main>
     );
